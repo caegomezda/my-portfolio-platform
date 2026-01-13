69109bc9027b5e2c541b0c8edc613a55dc634d94
caegomezda <camiloedg23@gmail.com>
2026-01-12 20:24:22 -0500

    Test Project loaded

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,27 @@
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// import { getSupabase } from "@/lib/supabaseClient";
+import { getSupabase } from "@/lib/supabaseClient";
 
-// export default async function Home() {
-//   const supabase = getSupabase();
+export default async function Home() {
+  const supabase = getSupabase();
 
-//   const { data, error } = await supabase.from("projects").select("*");
+  const { data, error } = await supabase.from("projects").select("*");
 
-//   if (error) {
-//     return (
-//       <div style={{ padding: 20 }}>
-//         <h1>Supabase error</h1>
-//         <pre>{JSON.stringify(error, null, 2)}</pre>
-//       </div>
-//     );
-//   }
+  if (error) {
+    return (
+      <div style={{ padding: 20 }}>
+        <h1>Supabase error</h1>
+        <pre>{JSON.stringify(error, null, 2)}</pre>
+      </div>
+    );
+  }
 
-//   return (
-//     <div style={{ padding: 20 }}>
-//       <h1>Projects loaded</h1>
-//       <pre>{JSON.stringify(data, null, 2)}</pre>
-//     </div>
-//   );
-// }
-
-export default function Home() {
-  return <h1>IT WORKS</h1>;
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Projects loaded</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
+
 
